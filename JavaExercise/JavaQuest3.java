ffe5dafe69e2974b372ce20c41f0bb8a58fdfb2e
fannyChoi <fanny922hk@gmail.com>
2025-04-13 21:56:13 +0800

    2156

--- JavaExercise/JavaQuest3.java
+++ JavaExercise/JavaQuest3.java
@@ -2,22 +2,22 @@ package JavaExercise;
 
 public class JavaQuest3 {
   public static void main(String[] args) {
-    int a = 10;
+    int a = 9;
     if (a == 9) {
       System.out.println("a is equals to 9");
-    } else if () {
+    } else if (a >= 0 && a <= 8) {
       System.out.println("a is between 0 and 8");
     // how about a > 9?
     } else {
       System.out.println("a < 0");
     }
 
-    String str = "I love Java. I love coding";
-    if () {
+    String str = "I love Java. I love coding!";
+    if (str.length() == 27) {
       System.out.println("str length=27");
-    } else if () { // code here ... add the condition
+    } else if (str.length() < 27) { // code here ... add the condition
       System.out.println("str length < 27");
-    } else if () { // code here ... complete the code
+    } else if (str.length() > 27) { // code here ... complete the code
       System.out.println("str length > 27");
     }
   }
